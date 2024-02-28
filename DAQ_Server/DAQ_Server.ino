0a497a564356f9abb3254bf066bdf024931ea7dc
jdc13 <118575992+jdc13@users.noreply.github.com>
2024-02-28 08:23:07 -0700

    Organization and Testing updates
    Organized files
    Continued development of DAQ Server
    CAD models for test sled and wench (still in progress)

--- DAQ_Server/DAQ_Server.ino
+++ DAQ_Server/DAQ_Server.ino
@@ -1,5 +1,3 @@
-
-
 /* DAQ_Server
 Created By:   Joshua Chapman
 Date:         2/21/24
@@ -8,9 +6,9 @@ Description:  This code collects data from several sensors and posts 2D point cl
 */
 
 /*To Do:
-- Set up wifi access point to transmit data
 - Have WiFi access point running on one core while the sensor data is running on the second core.
 - Verify Sensor rates
+- Maybe add start button using built in on pin 38?
 */
 
 //Hardware Libraries
@@ -22,8 +20,6 @@ Description:  This code collects data from several sensors and posts 2D point cl
 //Hardware Class Initializations
 TCA9548A      I2CMux;     //I2C Mux
 SFEVL53L1X    dist[8];    //Distance Sensor Array. Using 8 channels to keep the channel matching the array index. Only using 1,2,3,4,5,6
-
-
 
 //Data Transformation Variables
 float position = 0;
@@ -47,6 +43,47 @@ String Bank_L; //Main string to transmit sensor data from left bank
 bool lock_L = false; //Boolean to avoid data conflicts in dual core mode
 String buff_R; //Buffer for right (place data here if the main string is being used to transmit data)
 String buff_L; //Buffer for left (place data here if the main string is being used to transmit data)
+
+
+//Network Libraries
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+
+
+//Network Settings
+const char* SSID = "yourAP";
+// const char* Pswd = "1234"; //Using open network, no password
+
+//Initialize server
+WebServer server(80);
+
+void handleRoot() {
+  // digitalWrite(led, 1);
+  server.send(200, "text/plain",Bank_R + "\n" + Bank_L);
+  
+  Bank_R = "";
+  Bank_L = "";
+  // digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  // digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  // digitalWrite(led, 0);
+}
 
 
 
@@ -79,19 +116,35 @@ void setup() {
     start = millis();
   }
 
-    
+  //Configure WiFi
+  Serial.println("Initializing WiFi...");
+  WiFi.softAP(SSID);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Server IP Address: ");
+  Serial.println(IP);
+
+  server.begin();  
+  server.on("/", handleRoot);
 }
 
 
 void loop() {
+  server.handleClient();
+  delay(2);  
   
+    
+  
+
+
+
+
   //Check Distance Sensors
   //Left Bank
   for(int i = 1; i <=3; i++){
     I2CMux.openChannel(i);
     if(dist[i].checkForDataReady()){ //Check the sensor to see if it has data
       
-      float distance = dist[i].getDistance(); //Will need to change this line to feed data to the server
+      float distance = dist[i].getDistance()*1e-3; //get distance and convert to meters 
       if(!lock_R){
         Bank_R += String(distance * vectors[i][0] + position) + "," + String(distance * vectors[i][1]) + "\t";
       }
@@ -109,7 +162,7 @@ void loop() {
   for(int i = 4; i <=6; i++){
     I2CMux.openChannel(i);
     if(dist[i].checkForDataReady()){ //Check the sensor to see if it has data
-      float distance = dist[i].getDistance(); //Will need to change this line to feed data to the server
+      float distance = dist[i].getDistance() * 1e-3; //get distance and convert to meters 
       if(!lock_L){
         Bank_L += String(distance * vectors[i][0] + position) + "," + String(distance * vectors[i][1]) + "\t";  
       }
